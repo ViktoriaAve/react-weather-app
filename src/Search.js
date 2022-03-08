3871db548f171c624e06a09d46ac7695990f5959
ViktoriaAve <73614080+ViktoriaAve@users.noreply.github.com>
2022-03-08 18:24:34 +0200

    Added CSS, layout, forecast row

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Search() {
   let [city, setCity] = useState(" ");
@@ -9,12 +12,14 @@ export default function Search() {
   function showWeather(response) {
     setResult(true);
     setWeather({
+      name: response.data.name,
       temperature: response.data.main.temp,
       description: response.data.weather[0].description,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     });
+    console.log(response.data.name);
   }
 
   function handleSubmit(event) {
@@ -28,6 +33,12 @@ export default function Search() {
     setCity(event.target.value);
   }
 
+
+ 
+
+
+
+
   let form = (
     <form onSubmit={handleSubmit}>
       <input
@@ -35,7 +46,8 @@ export default function Search() {
         placeholder="Enter a city..."
         onChange={updateCity}
       />
-      <button type="submit"> Search </button>
+      <button type="submit" className="btn-search"> Search </button>
+      <button type="submit" className="btn-current"> Current </button>
     </form>
   );
 
@@ -43,16 +55,52 @@ export default function Search() {
     return (
       <div>
         {form}
-        <h2>{city}</h2>
+        <p className="text-strong"> {weather.name}, {Math.round(weather.temperature)}°C</p>
+        <p>{weather.description}</p>
+        
+        <Container>
+          <Row>
+            <Col>
+          <img src={weather.icon} alt="Weather Icon" className="weather-icon" />
+        </Col>
+        <Col>
         <ul>
-          <li>Temperature: {Math.round(weather.temperature)}°C </li>
-          <li>Description: {weather.description} </li>
           <li>Humidity: {weather.humidity}% </li>
           <li>Wind: {weather.wind}km/h </li>
-          <li>
-            <img src={weather.icon} alt="Weather Icon" />
-          </li>
         </ul>
+        </Col>
+        </Row>
+        </Container>
+
+        <Container className="weatherForecast">
+          <Row>
+            <Col>
+              <p>Mon</p>
+              <img src={weather.icon} alt="Weather Icon" />
+             <p>{weather.description}</p>
+            </Col>
+            <Col>
+              <p>Mon</p>
+              <img src={weather.icon} alt="Weather Icon" />
+             <p>{weather.description}</p>
+            </Col>
+            <Col>
+              <p>Mon</p>
+              <img src={weather.icon} alt="Weather Icon" />
+             <p>{weather.description}</p>
+            </Col>
+            <Col>
+              <p>Mon</p>
+              <img src={weather.icon} alt="Weather Icon" />
+             <p>{weather.description}</p>
+            </Col>
+            <Col>
+              <p>Mon</p>
+              <img src={weather.icon} alt="Weather Icon" />
+             <p>{weather.description}</p>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   } else {
